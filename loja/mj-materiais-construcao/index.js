d76add921580fa0c9c765cae60a19dc466a9c476
orgtarefas <orgtarefas.site@gmail.com>
2026-03-10 17:11:18 -0300

    Update index.js

--- loja/mj-materiais-construcao/index.js
+++ loja/mj-materiais-construcao/index.js
@@ -703,15 +703,19 @@ function renderizarPainelAgendamento() {
                                 <div class="servico-track">
                 `;
                 
-                // Adicionar cards para cada agendamento deste serviço
-                servico.itens.forEach((item, idx) => {
-                    const posicao = idx + 1;
+                // cards para cada agendamento deste serviço
+                const itensInvertidos = [...servico.itens].reverse();
+                
+                itensInvertidos.forEach((item, idx) => {
+                    // A posição ainda deve ser baseada na ordem original
+                    const posicaoOriginal = servico.itens.findIndex(i => i.senha === item.senha) + 1;
+                    
                     html += `
                         <div class="servico-card">
                             <div class="servico-tag-small" title="${item.servico_nome}">${item.servico_nome}</div>
                             <div class="senha-numero-small">${item.senha}</div>
                             <div class="senha-cliente-small">${item.cliente_nome}</div>
-                            <span class="senha-posicao-small">${posicao}° na fila</span>
+                            <span class="senha-posicao-small">${posicaoOriginal}° na fila</span>
                         </div>
                     `;
                 });
@@ -3123,6 +3127,7 @@ window.filtrarPorCategoria = filtrarPorCategoria;
 window.fecharModal = fecharModal;
 
 console.log("✅ index.js carregado com sucesso!");
+
 
 
 
