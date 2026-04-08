2d67fef1f12979b8ba0cbc335ee2764987764f39
orgtarefas <orgtarefas.site@gmail.com>
2026-04-08 13:31:33 -0300

    Update index_4.js

--- loja/mj-materiais-construcao/index_4.js
+++ loja/mj-materiais-construcao/index_4.js
@@ -2,6 +2,29 @@
 console.log("📁 Módulo 4 Carregado: Agendamento Escuta e Renderização");
 
 // ============================================
+// IMPORTAÇÕES DO FIREBASE
+// ============================================
+import { 
+    db, 
+    collection, 
+    doc, 
+    getDocs, 
+    getDoc, 
+    setDoc,
+    updateDoc,
+    deleteDoc,
+    addDoc,
+    query,
+    where,
+    orderBy,
+    onSnapshot,
+    serverTimestamp,
+    increment,
+    runTransaction,
+    limit
+} from './novo_firebase_config.js';
+
+// ============================================
 // INICIAR ESCUTA AGENDAMENTOS
 // ============================================
 function iniciarEscutaAgendamentos() {
