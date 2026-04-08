dff0dd3914cb0d669d1161225a7c4cf3a64b369f
orgtarefas <orgtarefas.site@gmail.com>
2026-04-08 13:31:47 -0300

    Update index_5.js

--- loja/mj-materiais-construcao/index_5.js
+++ loja/mj-materiais-construcao/index_5.js
@@ -2,6 +2,29 @@
 console.log("📁 Módulo 5 Carregado: Agendamento Carrossel e Paginação");
 
 // ============================================
+// IMPORTAÇÕES DO FIREBASE
+// ============================================
+import { 
+    db, 
+    collection, 
+    doc, 
+    getDocs, 
+    getDoc, 
+    setDoc,
+    updateDoc,
+    deleteDoc,
+    addDoc,
+    query,
+    where,
+    orderBy,
+    onSnapshot,
+    serverTimestamp,
+    increment,
+    runTransaction,
+    limit
+} from './novo_firebase_config.js';
+
+// ============================================
 // RENDERIZAR COLUNA OUTROS NA FILA
 // ============================================
 function renderizarColunaOutros(outrosNaFila, agendamentosPorServico, servicosOrdenados) {
